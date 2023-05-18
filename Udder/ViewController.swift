9e09102884913c48a529cee819b3b4112c9cf7f4
jpyang168@gmail.com <jpyang168@gmail.com>
2023-05-17 18:07:54 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jiping Yang on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
